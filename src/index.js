2706e45f305def77440a15597ee2eca2ba80f284
Alistair Smith <alistairsmith01+git@gmail.com>
2020-03-13 08:17:08 +0000

    Typescript (two)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import express from 'express';
 import helmet from 'helmet';
 import Session from 'express-session';
 
-class App extends express {
+class Express {
+  constructor(options) {
+    return express(options);
+  }
+}
+
+class App extends Express {
   constructor() {
     super();
+
     this.use(helmet());
     this.use(
       Session({
@@ -17,9 +24,8 @@ class App extends express {
   async getAuthorized(path, callback) {
     return this.get(path, (req, res, next) => {
       if (!req.session.authorized) {
-        return res
-          .status(401)
-          .json({error: true, status: 401, message: 'Unauthorized.'});
+        // Unauthorized!
+        return res.status(401).json({error: true, status: 401, message: 'Unauthorized.'});
       }
 
       callback(req, res, next);
@@ -28,3 +34,5 @@ class App extends express {
 }
 
 const app = new App();
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.listen(process.env.PORT || 8080);
