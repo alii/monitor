59984cc552c1ff5b2f150a22c73d8be50f774e49
washedDeveloper <patrick.brophy2@gmail.com>
2020-03-15 14:12:07 -0400

    getting products and adding them to the site

--- server/functions/Product.js
+++ server/functions/Product.js
@@ -1,4 +1,5 @@
 import Site from '../models/Site';
+import Product from '../models/Product';
 import Axios from 'axios';
 export class BasicSite {
   constructor(mongoID) {
@@ -6,6 +7,25 @@ export class BasicSite {
   }
 
   async getProducts() {
-    // TODO: Add this
+    let response = await Axios.get(this.site.url + '/products.json');
+    let siteProductArray = response.data.products;
+    let productArray = [Product];
+    for (let i = 0; i < siteProductArray.length; i++) {
+      let variantArray = [];
+      for (let j = 0; j < siteProductArray[i].variants.length; j++) {
+        let variant = siteProductArray[i].variants[j];
+        variantArray.push(variant);
+      }
+      productArray.push(
+        new Product.create({
+          variants: variantArray,
+          name: siteProductArray[i].title,
+          handle: siteProductArray[i].handle,
+          id: siteProductArray[i].id,
+        }),
+      );
+    }
+
+    this.site.products = productArray;
   }
 }
