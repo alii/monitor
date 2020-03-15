3b64bb84e8e2abb41d7bcdd35b8f161cccc52f17
Alistair Smith <alistairsmith01+git@gmail.com>
2020-03-15 18:20:01 +0000

    Pushing

--- server/functions/Product.js
+++ server/functions/Product.js
@@ -1,31 +1,34 @@
 import Site from '../models/Site';
 import Product from '../models/Product';
 import Axios from 'axios';
+
 export class BasicSite {
   constructor(mongoID) {
     this.site = Site.findOne({ _id: mongoID });
   }
 
   async getProducts() {
-    let response = await Axios.get(this.site.url + '/products.json');
-    let siteProductArray = response.data.products;
-    let productArray = [Product];
-    for (let i = 0; i < siteProductArray.length; i++) {
-      let variantArray = [];
-      for (let j = 0; j < siteProductArray[i].variants.length; j++) {
-        let variant = siteProductArray[i].variants[j];
-        variantArray.push(variant);
-      }
-      productArray.push(
-        new Product.create({
-          variants: variantArray,
-          name: siteProductArray[i].title,
-          handle: siteProductArray[i].handle,
-          id: siteProductArray[i].id,
-        }),
-      );
-    }
+    const response = await Axios.get(this.site.url + '/products.json');
+    const products = response.data.products.map(siteProduct => {
+      const variantArray = siteProduct.variants.map(variant => {
+        return {
+          name: variant.title,
+          price: variant.price,
+          lastUpdated: new Date(variant.updatedAt).getTime(),
+          id: variant.id,
+        };
+      });
 
-    this.site.products = productArray;
+      const product = new Product.create({
+        variants: variantArray,
+        name: siteProduct.title,
+        handle: siteProduct.handle,
+        id: siteProduct.id,
+      });
+
+      return product;
+    });
+
+    this.site.products = products;
   }
 }
