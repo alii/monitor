25df31e11e478c77764df35c3ac4dd1cecbd40c9
Alistair Smith <alistairsmith01+git@gmail.com>
2020-04-01 19:30:13 +0100

    Updoot prettier

--- server/functions/BasicSite.js
+++ server/functions/BasicSite.js
@@ -40,12 +40,13 @@ export class BasicSite {
     });
 
     this.site.products = products.filter(async _product => {
-      const productPage = await this.get(`${this.site.url}/products/${_product.handle}`);
-      const $ = cheerio.load(productPage.data);
+      const productPage = await this.get(`${this.site.url}/products/${_product.handle}`, {
+        headers: {
+          accept: 'application/json',
+        },
+      });
 
-      const script = $('script[data-product-json]');
-      const product = JSON.parse(script.text());
-      console.log(script);
+      const product = JSON.parse(productPage.data);
 
       if (this.site.products.find(loadedProduct => loadedProduct.id === product.id)) {
         // TODO: Compare product and if necessary notify and save
